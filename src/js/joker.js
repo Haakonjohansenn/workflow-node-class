dbffe9ce3fc8209ec94c8411ccc8ef7614daa926
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-10-25 11:08:19 +0200

    make joker promise

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,3 +1,7 @@
 import joker from "give-me-a-joke";
 
-joker.getRandomJokeOfTheDay(console.log);
+export function dadJoke() {
+  return new Promise(function (resolve, reject) {
+    joker.getRandomDadJoke(resolve);
+  });
+}
